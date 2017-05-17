f15ed14f6d501fcd95d2c840115891a09be6329b
huyenltnguyen <huyenltnguyen@outlook.com>
2017-05-17 19:54:01 +0700

    create SearchBar, VideoList and fetch data

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list.js';
+import API_KEY from './components/api_key';
+
+// Create a new component.
+// This component should produce some HTML
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+		});
+	}
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos}/>
+			</div>
+		);	
+	}	
+}
+
+// Take this component's generated HTML and put on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector(".container"));
