882f546ad94a9b9e19de8a5bd4d625d9b5802765
huyenltnguyen <huyenltnguyen@outlook.com>
2017-05-18 00:51:38 +0700

    display videos' thumbnail and title

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ class App extends Component {
 		super(props);
 		this.state = { videos: [] };
 
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+		YTSearch({key: API_KEY, term: 'javascript'}, (videos) => {
 			this.setState({ videos });
 		});
 	}
