34f4e8116034f535f4dd920d045daa31cbe00c5b
huyenltnguyen <huyenltnguyen@outlook.com>
2017-06-21 06:34:52 +0700

    update

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,13 +1,20 @@
+// jshint ignore: start
+
 import React from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
-	const videoItems = props.videos.map( (video) => {
-		return <VideoListItem key={video.etag} video={video} />
+	const videoItems = props.videos.map((video) => {
+		return (
+			<VideoListItem
+				onVideoSelect={props.onVideoSelect}
+				key={video.etag}
+				video={video} />
+		);
 	});
 
 	return (
-		<ul className="col-md-4 list-group">
+		<ul className='col-md-4 list-group'>
 			{videoItems}
 		</ul>
 	);
